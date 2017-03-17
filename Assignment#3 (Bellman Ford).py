379f3bc85b4bc5b4c708211d7f4e0f753c0f8c20
Aashutosh <AashuAyush@users.noreply.github.com>
2017-03-17 22:58:02 +0530

    Add files via upload

---
+++ Assignment#3 (Bellman Ford).py
@@ -0,0 +1,52 @@
+from collections import defaultdict
+import timeit
+
+class Graph:
+
+    def __init__(self,vertices):
+        self.V= vertices 
+        self.graph = [] 
+        
+    def addEdge(self,v1,v2,w):
+        self.graph.append([v1, v2, w])
+         
+    def printArr(self, minDistance):
+        print("Vertex     Minimum Distance from Origin")
+        for i in range(self.V):
+            print("%d \t\t %d" % (i, minDistance[i]))
+
+    def BellF(self, source):
+ 
+        minDistance = [float("Inf")] * self.V
+        minDistance[source] = 0
+  
+        for i in range(self.V - 1):        
+            for v1, v2, w in self.graph:
+                if minDistance[v1] != float("Inf") and minDistance[v1] + w < minDistance[v2]:
+                        minDistance[v2] = minDistance[v1] + w
+ 
+        for v1, v2, w in self.graph:
+                if minDistance[v1] != float("Inf") and minDistance[v1] + w < minDistance[v2]:
+                        print "Graph contains negative weight cycle"
+                        return
+                         
+        self.printArr(minDistance)
+
+v = input("Number of vertices:")
+e = input("Number of edges:")
+g = Graph(v)
+for i in range (e):
+    new_edge = raw_input()
+    new_edge = new_edge.split()
+    source=int(new_edge[0])
+    destination=int(new_edge[1])
+    cost=int(new_edge[2])
+    #destination = input()
+    #cost = input()
+    g.addEdge(source,destination,cost)
+start = timeit.default_timer() 
+
+g.BellF(0)
+stop = timeit.default_timer()
+
+print stop - start
